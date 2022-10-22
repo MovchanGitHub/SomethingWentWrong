59646fec47af6fe52dd4ee0da64832371760ea19
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-10-22 20:15:04 +0300

    Add Sample Isometric Tilemap

---
+++ SomethingWentWrong/Assets/Scripts/Players Scripts/IsometricCharacterRenderer.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class IsometricCharacterRenderer : MonoBehaviour
+{
+    public static readonly string[] idleDirections = { "idle N", "idle W", "idle S", "idle E" };
+    public static readonly string[] runDirections = { "run N", "run W", "run S", "run E" };
+
+    private Animator _animator;
+    private int lastDirection;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        string[] directionArray = null;
+
+        if (direction.magnitude < .01f)
+        {
+            directionArray = idleDirections;
+        }
+        else
+        {
+            directionArray = runDirections;
+            lastDirection = DirectionToIndex(direction, 4);
+        }
+        
+        //_animator.Play(directionArray[lastDirection]);
+        if (direction.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public static int DirectionToIndex(Vector2 dir, int sliceCount)
+    {
+        Vector2 normDir = dir.normalized;
+        float step = 360f / sliceCount;
+        float halfstep = step / 2;
+        float angle = Vector2.SignedAngle(Vector2.up, normDir);
+        angle += halfstep;
+        if (angle < 0) angle += 360;
+        float stepCount = angle / step;
+        return Mathf.FloorToInt(stepCount);
+    }
+}
--- SomethingWentWrong/Assets/Scripts/Players Scripts/IsometricPlayerMovementController.cs
+++ SomethingWentWrong/Assets/Scripts/Players Scripts/IsometricPlayerMovementController.cs
@@ -6,27 +6,66 @@ using UnityEngine;
 public class IsometricPlayerMovementController : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed = 1f;
-    //private IsometricCharacterRenderer isoRenderer;
+    private float movementSpeedInit = 1f;
+    private float movementSpeed;
+    private IsometricCharacterRenderer isoRenderer;
 
     private Rigidbody2D rbody;
-
+    
+    private Vector2 inputVector;
+    private float verticalInput;
+    private float horizontalInput;
+    
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
-        //isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
+
+    private void Start()
+    {
+        movementSpeed = movementSpeedInit;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            MaximizeSpeed();
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            MinimizeSpeed();
     }
 
     private void FixedUpdate()
     {
         Vector2 currentPos = rbody.position;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        //Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        CalculateIsometricMovement();
+        
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-        //isoRenderer.SetDirection(movement);
+        isoRenderer.SetDirection(movement);
         rbody.MovePosition(newPos);
+    }
+
+    private void CalculateIsometricMovement()
+    {
+        //Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = new Vector2(
+            horizontalInput + verticalInput, 
+            horizontalInput * -0.5f + 0.5f * verticalInput);
+    }
+
+    private void MaximizeSpeed()
+    {
+        movementSpeed = 2 * movementSpeedInit;
+    }
+    
+    private void MinimizeSpeed()
+    {
+        movementSpeed = movementSpeedInit;
     }
 }
