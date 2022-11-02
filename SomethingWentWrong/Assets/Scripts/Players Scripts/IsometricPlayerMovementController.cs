4068a7347f3290af2e7f724b2160336538a569cd
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-11-02 22:08:11 +0300

    Add Test Scene

--- SomethingWentWrong/Assets/Scripts/Players Scripts/IsometricPlayerMovementController.cs
+++ SomethingWentWrong/Assets/Scripts/Players Scripts/IsometricPlayerMovementController.cs
@@ -45,7 +45,7 @@ public class IsometricPlayerMovementController : MonoBehaviour
             verticalInput = Input.GetAxis("Vertical");
             horizontalInput = Input.GetAxis("Horizontal");
 
-            Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+            inputVector = new Vector2(horizontalInput, verticalInput);
             inputVector = Vector2.ClampMagnitude(inputVector, 1);
             Vector2 movement = inputVector * movementSpeed;
             Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
