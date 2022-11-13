ffb47412bbc6b9d65ca75d13c116b10135e3377a
venexene <venexene@gmail.com>
2022-11-13 18:25:39 +0300

    Update Attack

--- SomethingWentWrong/Assets/Scripts/Mining Scipts/AttackPoint.cs
+++ SomethingWentWrong/Assets/Scripts/Mining Scipts/AttackPoint.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class AttackPoint : MonoBehaviour
 {
     private Vector3 startPosition;
+    [SerializeField] private float attackRange = 0.5f;
+    [SerializeField] private int damage = 5;
+    public LayerMask damagableLayers;
+
+    [SerializeField] private float attackRate = 2f;
+    private float attackTimer = 0f;
 
     void Awake()
     {
@@ -23,5 +29,31 @@ public class AttackPoint : MonoBehaviour
             transform.localPosition = new Vector3(0.25f + startPosition.x * Mathf.Sign(Input.GetAxis("Horizontal")), startPosition.y, startPosition.z);
         }
 
+        if (Time.time >= attackTimer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                attackTimer = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        //Запуск анимации нужен тут
+
+
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, attackRange, damagableLayers);
+
+        foreach (Collider2D hitObject in hitObjects)
+        {
+            hitObject.GetComponent<Damagable>().doDamage(damage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
