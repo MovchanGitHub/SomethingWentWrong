6bce6d4fa125ed6b5091c33169cecd9bae1d1901
Stanislav Leonchik <leonstas1@mail.ru>
2022-11-13 18:52:15 +0300

    Add Resolution and Volume Management

--- SomethingWentWrong/Assets/Scripts/Menu Scripts/SettingsScript.cs
+++ SomethingWentWrong/Assets/Scripts/Menu Scripts/SettingsScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
+
 public class SettingsScript : MonoBehaviour
 {
 
@@ -14,10 +16,30 @@ public class SettingsScript : MonoBehaviour
         Settings.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isOpened)
+            ShowHideMenu();
+    }
     public void ShowHideMenu()
     {
         isOpened = !isOpened;
         Settings.GameObject().SetActive(isOpened);
     }
-    
+
+    public void SetVolume(double value)
+    {
+        
+    }
+    public void SetResolution(int value)
+    {
+        switch (value)
+        {
+            case 0: Screen.SetResolution(1280, 720, false); Debug.Log("heheheh"); break;
+            case 1: Screen.SetResolution(1920, 1080, false); break;
+            case 2: Screen.SetResolution(2560, 1440, false); break;
+            case 3: Screen.SetResolution(3840, 2160, false); break;
+                
+        }
+    }
 }
