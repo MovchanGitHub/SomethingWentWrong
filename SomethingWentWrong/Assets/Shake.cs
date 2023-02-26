1e6bcd208be7aba9e152237ae581039f2ac2692f
ArkhipovGeorgiy <115147288+ArkhipovGeorgiy@users.noreply.github.com>
2023-02-26 18:29:14 +0300

    Add Shake Effect (#71)

---
+++ SomethingWentWrong/Assets/Shake.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public Animator camAnim;
+
+    public void CamShake()
+    {
+        camAnim.SetTrigger("shake");
+    }
+}
+
