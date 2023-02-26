19aaa9aecfa4b4f32714a311a5181d7fdb33eba7
Palom-Porom <114829512+Palom-Porom@users.noreply.github.com>
2023-02-26 18:15:08 +0300

    Basic PreVersion of GameManager
    Just a "frame" as prefub to create such thing

---
+++ SomethingWentWrong/Assets/Scripts/Game Manager Script/SpawnSystemScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSystemScript : MonoBehaviour
+{
+    public static SpawnSystemScript instance { get; private set; }
+
+    public GameObject player;
+
+    public GameObject lightHouse;
+
+    public GameObject UI;
+
+    public bool isUIOpened;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            return;
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ActivateLightHouse();
+        }        
+    }
+
+    public void ActivateLightHouse()
+    {
+        //isLightHouseActive = true;
+        lightHouse.GetComponent<LightHouse>().active = true;
+    }
+
+    public void DeactivateLightHouse()
+    {
+        //isLightHouseActive = false;
+        lightHouse.GetComponent<LightHouse>().active = false;
+    }
+
+    public void GameOver(string message)
+    {
+        UI.GetComponent<DeathScreen>().ShowDeathScreen(message);
+
+        IsometricPlayerMovementController.Instance.IsAbleToMove = false;
+
+        // InventoryManager.instance.canBeOpened = false;
+
+        SurvivalManager.Instance.transform.gameObject.SetActive(false);
+
+        player.gameObject.SetActive(false);
+
+        instance.isUIOpened = true;
+    }
+}
