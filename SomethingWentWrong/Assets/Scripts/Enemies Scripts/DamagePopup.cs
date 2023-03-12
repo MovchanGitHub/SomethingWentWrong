e3374b701df816202156b681d2a605f73fc0e516
venexene <venexene@gmail.com>
2023-03-12 15:29:04 +0300

    Update Damage Popups

--- SomethingWentWrong/Assets/Scripts/Enemies Scripts/DamagePopup.cs
+++ SomethingWentWrong/Assets/Scripts/Enemies Scripts/DamagePopup.cs
@@ -7,12 +7,14 @@ public class DamagePopup : MonoBehaviour
 {
     private TextMeshPro textMesh;
     [SerializeField] private float disappearTimer;
+    private float actualDisapearTimer;
     private Color textColor;
     private Vector3 moveVector;
 
     private void Awake()
     {
         textMesh = transform.GetComponent<TextMeshPro>();
+
     }
 
     public void setup(int damageAmount)
@@ -20,16 +22,29 @@ public class DamagePopup : MonoBehaviour
         textMesh.SetText(damageAmount.ToString());
         textColor = textMesh.color;
 
-        moveVector = new Vector3(0.4f, 1) * 15f;
+        moveVector = new Vector3(Random.Range(0f, 1f), 1) * 15f;
+        actualDisapearTimer = disappearTimer;
     }
 
     private void Update()
     {
         transform.position += moveVector * Time.deltaTime;
+
+        if (actualDisapearTimer > disappearTimer/2)
+        {
+            float scaleIncreaseAmmount = 1f;
+            transform.localScale += Vector3.one * scaleIncreaseAmmount * Time.deltaTime;
+        }
+        else 
+        {
+            float scaleDecreaseAmmount = 1f;
+            transform.localScale -= Vector3.one * scaleDecreaseAmmount * Time.deltaTime;
+        }
+
         moveVector -= moveVector * 10f * Time.deltaTime;
 
-        disappearTimer -= Time.deltaTime;
-        if (disappearTimer < 0)
+        actualDisapearTimer -= Time.deltaTime;
+        if (actualDisapearTimer < 0)
         {
             float disappearSpeed = 3f;
             textColor.a -= disappearSpeed * Time.deltaTime;
