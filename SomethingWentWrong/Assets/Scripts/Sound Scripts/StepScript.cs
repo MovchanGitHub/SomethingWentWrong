8482b9b8da5e8d73d75ec5eebffce32d79d9f80b
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2023-03-22 20:08:20 +0300

    Fix Script

--- SomethingWentWrong/Assets/Scripts/Sound Scripts/StepScript.cs
+++ SomethingWentWrong/Assets/Scripts/Sound Scripts/StepScript.cs
@@ -6,20 +6,13 @@ public class StepScript : MonoBehaviour
 {
     private string _sprite;
     private AudioSource Settings;
-    public AudioClip Step1;
-    public AudioClip Step2;
-    public AudioClip Step3;
-    public AudioClip Step4;
-    public AudioClip Step5;
-    public AudioClip Step6;
-    private AudioClip[] _steps;
+    [SerializeField] private List<AudioClip> _steps;
     private int ind;
     
     // Start is called before the first frame update
     void Start()
     {
         Settings = GetComponent<AudioSource>();
-        _steps = new [] { Step1, Step2, Step3, Step4, Step5, Step6 };
         ind = 0;
     }
 
