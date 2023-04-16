d15f587b4cf5f7490c29d0024b72d3bfd5990533
TRCMKR <59124556+TRCMKR@users.noreply.github.com>
2023-04-16 22:06:38 +0300

    Fix Bugs and Add 3D Sounds (#135)
    Co-authored-by: MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>

--- SomethingWentWrong/Assets/Scripts/Adaptive Music/MusicVol.cs
+++ SomethingWentWrong/Assets/Scripts/Adaptive Music/MusicVol.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UIElements;
 
 public class MusicVol : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        audioMixer.SetFloat("GameVol", GetComponent<Slider>().value);
+    }
 
     public void OnChangeSlider(float value)
     {
