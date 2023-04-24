f95480b303b784da4fd9fee52f480220b7cd9c5a
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2023-04-24 21:52:41 +0300

    Fix Merging Issues

--- SomethingWentWrong/Assets/Scripts/Input System/InputSystem.cs
+++ SomethingWentWrong/Assets/Scripts/Input System/InputSystem.cs
@@ -35,7 +35,8 @@ public class InputSystem : MonoBehaviour
         inputActions["PlayerShoot"] = GM.PlayerMovement.GetComponentInChildren<PlayerWeaponScript>().Attack;
         inputActions["PlayerStopShooting"] = GM.PlayerMovement.GetComponentInChildren<PlayerWeaponScript>().StopAttack;
         inputActions["InventoryClose"] = GM.InventoryManager.OpenCloseInventory;
-        inputActions["EncyclopediaOpen"] = GM.UI.Encyclopedia.EncyclopediaScript.OpenCloseEncyclopedia;
+        if (!GM.IsTutorial)
+            inputActions["EncyclopediaOpen"] = GM.UI.Encyclopedia.EncyclopediaScript.OpenCloseEncyclopedia;
         inputActions["MenuOpen"] = GM.UI.GetComponent<InGameMenuScript>().EscapeIsPressed;
         
         playerInput.actions["Sprint"].started += inputActions["PlayerRun"];
@@ -46,7 +47,8 @@ public class InputSystem : MonoBehaviour
         playerInput.actions["Weapon Attack"].started += inputActions["PlayerShoot"];
         playerInput.actions["Weapon Attack"].canceled += inputActions["PlayerStopShooting"];
         playerInput.actions["Open Inventory"].started += inputActions["InventoryClose"];
-        playerInput.actions["Open Encyclopedia"].started += inputActions["EncyclopediaOpen"];
+        if (!GM.IsTutorial)
+            playerInput.actions["Open Encyclopedia"].started += inputActions["EncyclopediaOpen"];
         playerInput.actions["Open InGame Menu"].started += inputActions["MenuOpen"];
     }
 
@@ -85,7 +87,8 @@ public class InputSystem : MonoBehaviour
         playerInput.actions["Weapon Attack"].canceled -= inputActions["PlayerStopShooting"];
         
         playerInput.actions["Open Inventory"].started -= inputActions["InventoryClose"];
-        playerInput.actions["Open Encyclopedia"].started -= inputActions["EncyclopediaOpen"];
+        if (!GM.IsTutorial)
+            playerInput.actions["Open Encyclopedia"].started -= inputActions["EncyclopediaOpen"];
         playerInput.actions["Open InGame Menu"].started -= inputActions["MenuOpen"];
     }
 }
